815381dded254d251847bfa8ddce098e7ab4c59f
SFeri95 <somiferi95@gmail.com>
2021-02-10 11:47:07 +0100

    Második feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fuvar;
 
-/**
- *
- * @author FreakyFred
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Fuvar {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args) throws IOException {
+        
+        List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
     }
     
 }
